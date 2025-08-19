b55def2e1557e24d6c66478307d1bbab85ad3d8d
dharavathrajkumar22 <daravathmani6@gmail.com>
2025-08-19 10:45:52 +0530

    main

--- src/test/java/selenium/openingchrome.java
+++ src/test/java/selenium/openingchrome.java
@@ -3,6 +3,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class openingchrome {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Rajkumar\\IdeaProjects\\javacode\\chromedriver.exe");
-        ChromeDriver ob=new ChromeDriver();
+        ChromeDriver driver=new ChromeDriver();
     }
 }
